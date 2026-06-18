d370ad39e8c0c375e99662fe0ac88c19d24b1148
Roman Mondello <rmondello2629@gmail.com>
2026-06-17 22:27:35 -0400

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.ts
@@ -0,0 +1,53 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s: string, t: string): string {
+        if (t === "") return "";
+
+        let countT: Record<string, number> = {};
+
+        // Initialize `countT`
+        for (const char of t) {
+          countT[char] = (countT[char] ?? 0) + 1;
+        }
+
+        let window: Record<string, number> = {};
+        let have: number = 0;
+        let need: number = Object.keys(countT).length;
+        let res: [number, number] = [-1, -1];
+        let resLen: number = Infinity;
+        let left: number = 0;
+
+        for (let right = 0; right < s.length; right++) {
+          const char: string = s[right];    
+
+          window[char] = (window[char] ?? 0) + 1;
+
+          if (char in countT && window[char] === countT[char]) {
+            have++;
+          }
+
+          while (have === need) {
+            const idx: number = right - left + 1;
+            if (idx < resLen) {
+                console.log(left, right, idx)
+                res = [left, right];
+                resLen = idx;
+            }
+
+            const leftChar: string = s[left];
+            window[leftChar]--;
+
+            if (leftChar in countT && window[leftChar] < countT[leftChar]) {
+                have--;
+            }
+
+            left++;
+          }
+        }
+        return resLen === Infinity ? "" : s.slice(res[0], res[1] + 1);
+    }
+}
